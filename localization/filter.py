5e96d2ae343746b36a4b3e4b60889a9be741a7d2
John Wang <johnjwang1@gmail.com>
2011-04-08 01:23:42 -0700

    add kalman filter proof of concept and testing

--- localization/filter.py
+++ localization/filter.py
@@ -1,5 +1,58 @@
-import numpy
+import numpy as np
 import models, util
+
+class LinearSystem:
+  def __init__(self, C, R, Q):
+    '''
+    Models the system
+    x[k] = A[k] * x[k-1] + B[k] * u[k] + transition noise
+    z[k] = C[k] * x[k] + measurement noise
+    
+    R is the covariance of transition noise
+    Q is the covariance of measurement noise
+    '''
+    self.C = np.array(C)
+    self.R = np.array(R)
+    self.Q = np.array(Q)
+  def A(self, x):
+    raise NotImplementedError()
+  def B(self, x):
+    raise NotImplementedError()
+
+class KalmanFilter:
+  def __init__(self, system, state, covariance):
+    self.system = system
+    self.x = np.array(state)
+    self.sigma = np.array(covariance)
+
+  def elapseTime(self, control):
+    A = np.array(self.system.A(self.x))
+    B = np.array(self.system.B(self.x))
+    u = np.array(control)
+
+    # Calculate new mean and covariance
+    x = array_multiply(A, self.x) + array_multiply(B, u)
+    sigma = array_multiply(A, self.sigma, A.T) + self.system.R
+
+    # Update belief
+    self.x = x
+    self.sigma = sigma
+
+  def observe(self, observation):
+    z = np.matrix(observation).T
+    C = np.matrix(self.system.C)
+    Q = np.matrix(self.system.Q)
+    x = np.matrix(self.x).T
+    sigma = np.matrix(self.sigma)
+
+    # Calculate Kalman gain and new state
+    K = sigma * C.T * (C*sigma*C.T + Q).I
+    x = x + K * (z - C * x)
+    sigma = (np.eye(3) - K*C) * sigma
+
+    # Update belief
+    self.x = np.array(x.T)[0]
+    self.sigma = np.array(sigma)
 
 class ParticleFilter:
   def __init__(self, numParticles=1000):
@@ -41,8 +94,8 @@ class ParticleFilter:
     for particle in oldBeliefs.keys():
       for i in range(int(oldBeliefs[particle] + 0.5)):
         # TODO tune sigma (2nd parameter)
-        dx, dy = numpy.random.normal(0, 10.0, 2)
-        dtheta = numpy.random.normal(0, 0.5)
+        dx, dy = np.random.normal(0, 10.0, 2)
+        dtheta = np.random.normal(0, 0.5)
         
         # Account for motion
         if motion is not None:
@@ -57,20 +110,23 @@ class ParticleFilter:
     return self.particles
   
   def getEstimate(self):
-    estimate = numpy.array((0,0,0))
+    estimate = np.array((0,0,0))
     totalCount = 0
     for particle in self.particles.keys():
-      count = numpy.array(self.particles[particle])
+      count = np.array(self.particles[particle])
       totalCount += count
       estimate += count * particle
     estimate /= totalCount
     return tuple(estimate)
   
 def sample_uniform(n, xmin, xmax, ymin, ymax):
-  x = (xmax - xmin) * numpy.random.random_sample((n,)) + xmin
-  y = (ymax - ymin) * numpy.random.random_sample((n,)) + ymin
+  x = (xmax - xmin) * np.random.random_sample((n,)) + xmin
+  y = (ymax - ymin) * np.random.random_sample((n,)) + ymin
   # theta = heading from [-pi,pi]
-  t = 2*numpy.pi * numpy.random.random_sample((n,)) - numpy.pi
+  t = 2*np.pi * np.random.random_sample((n,)) - np.pi
   
   # Return n tuples of (x,y) pairs
   return [(x[i], y[i], t[i]) for i in xrange(n)]
+
+def array_multiply(*args):
+  return reduce(np.dot, args)
