6d09a408452749cf3b2386f9fa1bcfb06ab64800
DELL <karts@ubuntu.ubuntu-domain>
2011-06-22 12:21:48 -0700

    Reverting to old ballbotmotion.py

--- pathing/ballbotmotion.py
+++ pathing/ballbotmotion.py
@@ -317,7 +317,7 @@ class Ballbot(Robot):
                 angle = struct.unpack("<h",angle)
                 angle = angle[0]
                 
-		#print "distance ",distance," angle ",angle," distance travelled ", self.distancetravelled, " goal ", d
+		print "distance ",distance," angle ",angle," distance travelled ", self.distancetravelled, " goal ", d
 
                 # update position based on sensor readings
                 y = self.position[1] + distance*math.cos(math.radians(angle));
@@ -335,7 +335,7 @@ class Ballbot(Robot):
                 self.distancetravelled = self.distancetravelled + distance
             	self.tickssofar = self.tickssofar + ticks
             if(encoderticks_to_distance(self.tickssofar) < d):
-		if(started == 0):
+                if(started == 0):
 		    started = 1	
                     self.turnRight()
             else:
