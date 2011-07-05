a506f10ec645c65a7e9f538f843fabbb67b0fba0
John Wang <johnjwang1@gmail.com>
2011-07-05 16:28:29 -0700

    Add corner detection (buggy)

--- vision/court/findlines.py
+++ vision/court/findlines.py
@@ -1,4 +1,5 @@
-import sys, math
+import sys
+import numpy as np
 import cv
 
 def cvOpen(src, dst, element):
@@ -30,8 +31,8 @@ def normalize_line(line):
     r, theta = line
     if r < 0:
         r = -r
-        theta += math.pi
-    return (r, theta % (2*math.pi))
+        theta += np.pi
+    return (r, theta % (2*np.pi))
 
 def avg_lines(lines):
     '''
@@ -39,8 +40,8 @@ def avg_lines(lines):
     Returns average (r, theta)
     '''
     r = sum(x[0] for x in lines) / len(lines)
-    theta = math.atan2(sum(math.sin(x[1]) for x in lines),
-                       sum(math.cos(x[1]) for x in lines))
+    theta = np.arctan2(sum(np.sin(x[1]) for x in lines),
+                       sum(np.cos(x[1]) for x in lines))
     return normalize_line((r, theta))
 
 def angle_difference(a, b):
@@ -48,7 +49,39 @@ def angle_difference(a, b):
     Computes the angle difference (a - b), normalized to [-pi, pi)
     '''
     diff = a - b
-    return (diff + math.pi) % (2*math.pi) - math.pi
+    return (diff + np.pi) % (2*np.pi) - np.pi
+
+def to_xy(r_theta):
+    '''
+    Converts (r, theta) to (x, y)
+    '''
+    r, theta = r_theta
+    return (r * np.cos(theta), r * np.sin(theta))
+
+def line_intersection(line1, line2):
+    '''
+    Finds the intersection point of two 2D lines in (r, theta) form,
+     where the line is parallel to the vector denoted by (r, theta)
+    Returns intersection point as (x, y)
+    See http://mathworld.wolfram.com/Line-LineIntersection.html
+    '''
+    x1, y1 = to_xy(line1)
+    x2, y2 = x1-y1, x1+y1
+    x3, y3 = to_xy(line2)
+    x4, y4 = x3-y3, x3+y3
+
+    print x1, y1
+    print x3, y3
+
+    a = np.linalg.det([[x1, y1], [x2, y2]])
+    b = np.linalg.det([[x3, y3], [x4, y4]])
+    d = np.linalg.det([[x1-x2, y1-y2], [x3-x4, y3-y4]])
+    x = np.linalg.det([[a, x1-x2], [b, x3-x4]]) / d
+    y = np.linalg.det([[a, y1-y2], [b, y3-y4]]) / d
+
+    print 'center of', line1, line2, 'is', (x,y)
+
+    return (x, y)
 
 def find_lines(frame):
     # Resize to 640x480
@@ -64,30 +97,22 @@ def find_lines(frame):
     cv.CvtColor(frame_small, frame_gray, cv.CV_BGR2GRAY)
 
     # Use color thresholding to get white lines
-    edges = cv.CreateImage(frame_size, cv.IPL_DEPTH_8U, 1)
-    cv.Threshold(frame_gray, edges, 200, 255, cv.CV_THRESH_BINARY)
+    threshold = cv.CreateImage(frame_size, cv.IPL_DEPTH_8U, 1)
+    cv.Threshold(frame_gray, threshold, 200, 255, cv.CV_THRESH_BINARY)
 
     openElement = cv.CreateStructuringElementEx(11,11,5,5,
                                                 cv.CV_SHAPE_RECT)
     closeElement = cv.CreateStructuringElementEx(21, 21, 10, 10,
                                                  cv.CV_SHAPE_RECT)
-    cvOpen(edges, edges, openElement)
-    cvClose(edges, edges, closeElement)
+    cvOpen(threshold, threshold, openElement)
+    cvClose(threshold, threshold, closeElement)
     
-    
-    # Use Canny edge detection to get lines
-    cv.Canny(edges, edges, 400, 400)
-    """
-    cv.Dilate(edges, edges,
-              cv.CreateStructuringElementEx(3, 3, 0, 0, 
-                                            cv.CV_SHAPE_RECT))
-    cv.Erode(edges, edges,
-              cv.CreateStructuringElementEx(1, 1, 0, 0, 
-                                            cv.CV_SHAPE_RECT))
-    """
+    # Use Canny edge detection to find edges
+    edges = cv.CreateImage(frame_size, cv.IPL_DEPTH_8U, 1)
+    cv.Canny(threshold, edges, 400, 400)
 
+    # Use Hough transform to find equations for lines
     line_storage = cv.CreateMemStorage()
-
     probabilistic = False
     if probabilistic:     # Probabilistic
         lines = cv.HoughLines2(edges, line_storage, cv.CV_HOUGH_PROBABILISTIC,
@@ -133,7 +158,7 @@ def find_lines(frame):
         for i in range(len(lines)):
             print lines[i]
             rho, theta = lines[i]
-            a, b = math.cos(theta), math.sin(theta)
+            a, b = np.cos(theta), np.sin(theta)
             x0, y0 = a*rho, b*rho
             pt1 = (cv.Round(x0 + 1000*(-b)),
                    cv.Round(y0 + 1000*(a)))
@@ -165,11 +190,57 @@ def find_lines(frame):
                     line1, closest = closest, line1
             # Make a tuple (line1, line2) or (line, None) if no match found
             grouped_lines.append((line1, closest))
-
         print 'Pairs of lines:', grouped_lines
 
+        # If 2+ pairs of lines, find corners (intersection point of lines)
+        if len(grouped_lines) > 1:
+          x, y = 0, 0
+          count = 0
+          for i in range(len(grouped_lines)):
+            pair1 = grouped_lines[i]
+            for j in range(i+1, len(grouped_lines)): 
+              pair2 = grouped_lines[j]
+              pts = [line_intersection(pair1[0], pair2[0]),
+                     line_intersection(pair1[0], pair2[1]),
+                     line_intersection(pair1[1], pair2[0]),
+                     line_intersection(pair1[1], pair2[1])]
+              for pt in pts:
+                x += pt[0]
+                y += pt[1]
+                count += 1
+          x /= count
+          y /= count
+          print 'Intersection:', (x, y)
+          pt = cv.Round(x), cv.Round(y)
+          cv.Circle(frame_small, pt, 4, (0,255,0,0))
+
+          # Find direction of intersection by following each line
+          angles = []
+          for pair in grouped_lines:
+            angles.append(pair[0][1] + cv.CV_PI/2)
+            angles.append(pair[0][1] - cv.CV_PI/2)
+          for angle in angles:
+            # Look 50px down the line for white pixels
+            # TODO look a variable amount
+            x1 = x + 50*np.cos(angle)
+            y1 = y + 50*np.sin(angle)
+            # Enforce limits
+            x1 = min(max(0, x1), frame_size[0])
+            y1 = min(max(0, y1), frame_size[1])
+            srchPt = cv.Round(x1), cv.Round(y1)
+            print srchPt, threshold[srchPt]
+
+            if threshold[srchPt] == 0:
+              x1 = x + 50*np.cos(angle + cv.CV_PI)
+              y1 = y + 50*np.sin(angle + cv.CV_PI)
+              invSrchPt = cv.Round(x1), cv.Round(y1)
+              cv.Line(frame_small, pt, invSrchPt,
+                      (0,255,0,0), 1, 8)
+              print 'Angle:', angle+cv.CV_PI
+              break
+
     cv.ShowImage('frame', frame_small)
-    cv.ShowImage('edges', edges)
+    cv.ShowImage('edges', threshold)
 
 def main():
     cv.NamedWindow('frame', cv.CV_WINDOW_AUTOSIZE)
