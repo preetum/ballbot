3e02f09569875a98bf5044ef18cccbb18bcf20fa
John Wang <johnjwang@berkeley.edu>
2011-08-04 14:06:19 -0700

    Bugfixes for ros_arduino_interface, rename Velocity1D to DriveCmd

--- ros_packages/ros_arduino_interface/nodes/ros_arduino_interface.py
+++ ros_packages/ros_arduino_interface/nodes/ros_arduino_interface.py
@@ -3,24 +3,23 @@
 import roslib; roslib.load_manifest('ros_arduino_interface')
 import rospy
 import robot
-from bb_msgs.msg import Velocity1D
-ballBot = None
+from bb_msgs.msg import DriveCmd
 
-def recievedDrivePacket(drivePacket): #this function is called when the listener catches a message
-    speed = drivePacket.linear
-    angle = drivePacket.steerAngle
-    #rospy.loginfo("Speed %d Angle %d", speed, angle)
-    ballBot.set_Velocity(speed, angle)
-    rospy.loginfo("Sent speeds to arduino")
+def recievedDrivePacket(ballBot, drivePacket):
+    '''
+    This function is called when the listener catches a message
+    '''
+    speed = int(drivePacket.velocity * 100)
+    angle = int(drivePacket.steerAngle * 180 / 3.141592654)
+    ballBot.set_velocity(speed, angle)
+    rospy.loginfo("Speed %d Angle %d" % (speed, angle))
 
 def initializer():
-    global ballBot
     ballBot = robot.BaseController()
-    rospy.init_node('ros_arduino_interface', anonymous=Fals0e)
-    listener = rospy.Subscriber("vel_cmd", Velocity1D, recievedDrivePacket) 
+    rospy.init_node('ros_arduino_interface', anonymous=True)
+    rospy.Subscriber("vel_cmd", DriveCmd,
+                     lambda pkt: recievedDrivePacket(ballBot, pkt))
+    rospy.spin()
 
 if __name__ == '__main__':
-    try:
-        initializer()
-    except rospy.ROSInterruptException: pass
-
+    initializer()
