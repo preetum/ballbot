8bbcf962ccdd6e0e122fea14da8ed09900d29ec7
John Wang <johnjwang1@gmail.com>
2011-08-03 20:38:09 -0700

    Add new robot.py class for new message formats

--- robot.py
+++ robot.py
@@ -1,37 +1,25 @@
 import serial
-import sys, re, struct, time
+import sys, re, struct, time, thread
 
-camera_params = {'height': 33.5,  # in cm
-  'offset_x': -4.5,  # offset from center of front axle, in cm
-  'offset_y': -10,
-  'tilt':  1.228,   # in radians
-  'resolution': (640, 480),
-  'radians_per_px': 0.0016
-}
-
-class Robot:
+class BaseController:
   '''
   Encapsulates the hardware functionality of the robot.
   Communicates with the Arduino and any other peripherals.
   '''
   START_BYTE = 0xFF
-  COMMAND_BYTE = 0x42
-  DATA_REQUESTED_BYTE = 0x21
+  CMD_SET_RAW = 0x41
+  CMD_SET_VELOCITY = 0x42
+  CMD_SET_PICKUP = 0x43
+  CMD_SET_PID = 0x44
+  CMD_GET_ODOMETRY = 0x61
   
   SERVO_LEFT = 57
   SERVO_CENTER = 92
   SERVO_RIGHT = 127
   
-  # Motor deadband: 91-99
-  MOTOR_FULL_FORWARD = 0
-  MOTOR_MIN_FORWARD = 91
-  MOTOR_NEUTRAL = 95
-  MOTOR_MIN_REVERSE = 99
-  MOTOR_FULL_REVERSE = 180
+  MAX_VELOCITY = 3200
 
-  MAX_VELOCITY = 300
-
-  def __init__(self, port='/dev/ttyUSB0'):
+  def __init__(self, port='/dev/ttyACM0'):
   
     # Initialize Arduino COM port
     try:
@@ -40,12 +28,12 @@ class Robot:
       print "No Arduino connected."
     
     # Initialize Arduino command packet
-    # Command packet format: 
+    # Command packet format:
     # > = big endian
     # B = unsigned byte
     # H = unsigned short (2 bytes)
-    self.cmd_packet_format = struct.Struct('>BHHBB')
-    self.cmd_packet = [Robot.COMMAND_BYTE, 0, 0, 0, 0]
+    self.cmd_packet_format = struct.Struct('>BBH')
+    self.cmd_packet = [BaseController.CMD_SET_RAW, 0, 0]
     
     # Wait for Arduino to initialize, then set initial values
     time.sleep(2)
@@ -56,23 +44,21 @@ class Robot:
     self.set_drive(0)
     self.refresh()
 
+  def set_velocity(self, linear, angular):
+    data_string = struct.pack('>Bhh', BaseController.CMD_SET_VELOCITY, linear, angular)
+    self.send_arduino_packet(data_string)
+
+  def set_pid(self, kp, ki, kd):
+    kp = int(kp * 100)
+    ki = int(ki * 100)
+    kd = int(kd * 100)
+    data_string = struct.pack('>Bhhh', BaseController.CMD_SET_PID, kp, ki, kd)
+    self.send_arduino_packet(data_string)
+
   def refresh(self):
     # Send data commands
     data_string = self.cmd_packet_format.pack(*self.cmd_packet)
     self.send_arduino_packet(data_string)
-
-    # Get odometry data
-    self.send_arduino_packet(chr(self.DATA_REQUESTED_BYTE))
-    # Parse results
-    b = self.serial.read()
-    if b and ord(b) == Robot.START_BYTE:
-      s = self.serial.read(6)
-      if len(s) == 6:
-        counts, angle, angular_velocity = struct.unpack(">hhh", s)
-        print 'counts: %d\t' % counts,
-        print 'angle: %.2f deg\t' % (angle * 180.0 / 32768),
-        print 'angular velocity: %.2f deg/s' % (angular_velocity * 180.0 / 32768)
-    
     
   def send_arduino_packet(self, data_string):
     '''
@@ -96,7 +82,7 @@ class Robot:
     checksum = length ^ reduce(lambda x,y: x^y, bytearray(data_string), 0)
     
     # Construct full packet
-    string = chr(Robot.START_BYTE) + chr(length) + \
+    string = chr(BaseController.START_BYTE) + chr(length) + \
               data_string + chr(checksum)
     
     self.serial.write(string)
@@ -109,51 +95,33 @@ class Robot:
     
     NOTE: actual angle => servo values have not been calibrated!
     '''
-    value = angle + Robot.SERVO_CENTER
+    value = angle + BaseController.SERVO_CENTER
     
     # check that output value is in valid range
-    if value > Robot.SERVO_RIGHT:
-      value = Robot.SERVO_RIGHT
-    elif value < Robot.SERVO_LEFT:
-      value = Robot.SERVO_LEFT
+    if value > BaseController.SERVO_RIGHT:
+      value = BaseController.SERVO_RIGHT
+    elif value < BaseController.SERVO_LEFT:
+      value = BaseController.SERVO_LEFT
     
     self.cmd_packet[1] = int(value)
 
-  def set_velocity(self, velocity):
+  def set_drive(self, velocity):
     '''
     Set the velocity setpoint to the given value in cm/s.
     '''
     if velocity < 0:
 	velocity = 0
-    elif velocity > Robot.MAX_VELOCITY:
-	velocity = Robot.MAX_VELOCITY
+    elif velocity > BaseController.MAX_VELOCITY:
+	velocity = BaseController.MAX_VELOCITY
     self.cmd_packet[2] = int(velocity)
-    
-  def set_drive(self, duty_cycle):
-    '''
-    Set the drive motor to the given % duty cycle, ranging from -100 to 100.
-    0 is stop, a positive value is forward, and a negative value is reverse
-    
-    if duty_cycle == 0:
-      value = Robot.MOTOR_NEUTRAL
-    else:
-      if duty_cycle > 0:
-        value = duty_cycle * (Robot.MOTOR_FULL_FORWARD -
-                  Robot.MOTOR_MIN_FORWARD) / 100 + Robot.MOTOR_MIN_FORWARD
-      else:   # duty_cycle < 0
-        value = -duty_cycle * (Robot.MOTOR_FULL_REVERSE - 
-                  Robot.MOTOR_MIN_REVERSE) / 100 + Robot.MOTOR_MIN_REVERSE
-                  
-    # check that output value is in valid range
-    if value < 0:
-      value = 0
-    elif value > 1023:
-      value = 1023
-    
-    value = duty_cycle
-    self.cmd_packet[2] = int(value)
-    ''' 
-    pass
+
+def update_thread(serial):
+  # Print from serial terminal
+  while True:
+    b = serial.read(100)
+    if b:
+      print b,
+    time.sleep(0.5)
 
 def main():
   '''
@@ -163,20 +131,34 @@ def main():
   Optional argument: com port
   '''
   if len(sys.argv) > 1:
-    robot = Robot(sys.argv[1])
+    robot = BaseController(sys.argv[1])
   else:
-    robot = Robot()
+    robot = BaseController()
+
+  velocity_values = [0, 0]
+  pid_values = [0, 0, 0]
+
+  thread.start_new_thread(update_thread, (robot.serial,))
 
   try:
     while (True):
-      robot.refresh()
+      line = sys.stdin.readline().strip()
+      if len(line) > 0:
+        if re.match('vel', line):
+          line = line[3:]
+          values = [int(v) for v in line.split(',')]
+          for i in range(min(len(values), 2)):
+            velocity_values[i] = values[i]
+            robot.set_velocity(*velocity_values)
+          print 'velocity:', velocity_values
+        elif re.match('pid', line):
+          line = line[3:]
+          values = [float(v) for v in line.split(',')]
+          for i in range(min(len(values), 3)):
+            pid_values[i] = values[i]
+            robot.set_pid(*pid_values)
+          print 'pid constants:', pid_values
           
-      line = sys.stdin.readline()
-      if len(line.strip()) > 0:
-        values = [float(v) for v in line.split(',')]
-        for i in range(min(len(values), 4)):
-          if i == 0: robot.set_steering(values[i])
-          elif i == 1: robot.set_velocity(values[i])
   except:
     # In case of error or Ctrl-C, zero motor values
     robot.reset()
