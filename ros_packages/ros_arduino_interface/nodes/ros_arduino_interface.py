80c57dbbf6910c02947363a478f68d07415bfe77
Ankush Gupta <ankush.me@hotmail.com>
2011-08-06 13:59:16 -0700

    IMU communicator added

--- ros_packages/ros_arduino_interface/nodes/ros_arduino_interface.py
+++ ros_packages/ros_arduino_interface/nodes/ros_arduino_interface.py
@@ -4,8 +4,9 @@ import roslib; roslib.load_manifest('ros_arduino_interface')
 import rospy
 import robot
 from bb_msgs.msg import DriveCmd
+import imu_communicator
 
-def recievedDrivePacket(ballBot, drivePacket):
+def recieved_drive_packet(ballBot, drivePacket):
     '''
     This function is called when the listener catches a message
     '''
@@ -14,12 +15,19 @@ def recievedDrivePacket(ballBot, drivePacket):
     ballBot.set_velocity(speed, angle)
     rospy.loginfo("Speed %d Angle %d" % (speed, angle))
 
+def send_heading_to_arduino(ballbot, imu):
+    waiter = rospy.rate(60)
+    while True:
+        ballbot.sync_odometry(imu.headingBAMS)
+        waiter.sleep
+
 def initializer():
     ballBot = robot.BaseController()
     rospy.init_node('ros_arduino_interface', anonymous=True)
     rospy.Subscriber("vel_cmd", DriveCmd,
-                     lambda pkt: recievedDrivePacket(ballBot, pkt))
-    rospy.spin()
+                     lambda pkt: recieved_drive_packet(ballBot, pkt))
+    imu = imu_communicator.IMU() # Initialize IMU
+    send_heading_to_arduino(ballbot, imu)
 
 if __name__ == '__main__':
     initializer()
