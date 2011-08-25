4337e7db9b315feeaa594f392638af15d7fbdf77
John Wang <johnjwang1@gmail.com>
2011-08-24 17:24:33 -0700

    Merge changes from Arduino Mega commit manually

--- demos/test/show_video.c
+++ demos/test/show_video.c
@@ -2,7 +2,7 @@
 
 #include <highgui.h>
 int main( int argc, char** argv ) {
-  cvNamedWindow( "Example2", CV_WINDOW_AUTOSIZE );
+  //  cvNamedWindow( "Example2", CV_WINDOW_AUTOSIZE );
 
   CvCapture* capture = cvCreateCameraCapture(0) ;
   IplImage* frame;
@@ -11,11 +11,11 @@ int main( int argc, char** argv ) {
     frame = cvQueryFrame( capture );
     if( !frame ) break;
 
-    cvShowImage( "Example2", frame );
-    char c = cvWaitKey(33);
-    if( c == 27 ) break;
+    //cvShowImage( "Example2", frame );
+    //char c = cvWaitKey(33);
+    //if( c == 27 ) break;
 
   }
   cvReleaseCapture( &capture );
-  cvDestroyWindow( "Example2" );
+  //cvDestroyWindow( "Example2" );
 }
