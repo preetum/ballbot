d0c732a5a93878ac501bb47af8f204e944f799b2
John Wang <johnjwang1@gmail.com>
2012-04-27 00:06:10 -0700

    Add pose viewer estimate vs ground truth view

--- ros_packages/court_localization/nodes/pose_viewer.py
+++ ros_packages/court_localization/nodes/pose_viewer.py
@@ -2,14 +2,94 @@
 import roslib; roslib.load_manifest('court_localization')
 import rospy
 from bb_msgs.msg import Pose
-from odometry_viewer import SingleSimulator
+from particle_viewer import Simulator
 
 import random, time, thread, sys
 import numpy as np
 
+last_true_pose = None
+
+class SingleSimulator(Simulator):
+  def __init__(self, title='Pose', trails=False):
+    super(SingleSimulator, self).__init__(title)
+    self.trails = trails
+    self.pose_lines = []
+    self.truth_lines = []
+
+  def draw_pose(self, pose, color='blue'):
+    l, c, r = self.get_points(pose)
+
+    if self.trails:
+      # Draw new lines
+      self.canvas.create_line(l[0], l[1], c[0], c[1], fill=color)
+      self.canvas.create_line(r[0], r[1], c[0], c[1], fill=color)
+    else:
+      # Move old lines
+      if not self.pose_lines:
+        self.pose_lines.append(
+          self.canvas.create_line(l[0], l[1], c[0], c[1], fill=color))
+        self.pose_lines.append(
+          self.canvas.create_line(r[0], r[1], c[0], c[1], fill=color))
+      else:
+        self.canvas.coords(self.pose_lines[0], l[0], l[1], c[0], c[1])
+        self.canvas.itemconfig(self.pose_lines[0], fill=color)
+        self.canvas.coords(self.pose_lines[1], r[0], r[1], c[0], c[1])
+        self.canvas.itemconfig(self.pose_lines[1], fill=color)
+
+  def draw_truth(self, pose, color='orange'):
+    l, c, r = self.get_points(pose)
+
+    if self.trails:
+      # Draw new lines
+      self.canvas.create_line(l[0], l[1], c[0], c[1], fill=color)
+      self.canvas.create_line(r[0], r[1], c[0], c[1], fill=color)
+    else:
+      # Move old lines
+      if not self.truth_lines:
+        self.truth_lines.append(
+          self.canvas.create_line(l[0], l[1], c[0], c[1], fill=color))
+        self.truth_lines.append(
+          self.canvas.create_line(r[0], r[1], c[0], c[1], fill=color))
+      else:
+        self.canvas.coords(self.truth_lines[0], l[0], l[1], c[0], c[1])
+        self.canvas.coords(self.truth_lines[1], r[0], r[1], c[0], c[1])
+
+
+  def get_points(self, belief, size=4):
+    beliefs = np.array([belief])
+    points = self.transform(beliefs[:,0:2])
+    angles = beliefs[:,2] - np.pi/2
+    A = np.cos(angles)
+    B = np.sin(angles)
+    R = np.array([[A, B], [-B, A]])
+
+    lowerLeft = points + np.dot([-size,size], R).T
+    center = points + np.dot([0,-size], R).T
+    lowerRight = points + np.dot([size,size], R).T
+
+    l = lowerLeft[0]
+    c = center[0]
+    r = lowerRight[0]
+
+    return (l, c, r)
+
 def msg_callback(msg, sim):
-  belief = np.array([msg.x, msg.y, msg.theta])
-  sim.refresh(belief, trails=True)
+  pose = np.array([msg.x, msg.y, msg.theta])
+  color = 'blue'
+
+  # compare estimated pose to last true; draw in red if off
+  if last_true_pose is not None:
+    diff = pose - last_true_pose
+    dist = np.linalg.norm(diff[0:1])
+    if dist > 10:  # in cm
+      color = 'red'
+  sim.draw_pose(pose, color)
+
+def truth_msg_callback(msg, sim):
+  pose = np.array([msg.x, msg.y, msg.theta])
+  global last_true_pose
+  last_true_pose = pose
+  sim.draw_truth(pose)
 
 def spin_thread(sim):
   # While ROS is alive, wait for messages
@@ -19,14 +99,17 @@ def spin_thread(sim):
 
 def main():
   # Get config
-  topic_name = rospy.get_param('~topic', 'pose')
+  topic_name = rospy.get_param('~pose', 'pose')
+  truth_topic_name = rospy.get_param('~true', 'pose_true')
 
-  sim = SingleSimulator('Pose')
+  sim = SingleSimulator(trails=False)
 
   # Initialize ROS listener
   rospy.init_node('pose_viewer', anonymous=True)
   rospy.Subscriber(topic_name, Pose, lambda msg: msg_callback(msg, sim),
                    queue_size=1)
+  rospy.Subscriber(truth_topic_name, Pose,
+                   lambda msg: truth_msg_callback(msg, sim), queue_size=1)
 
   # Start rospy spin thread
   thread.start_new_thread(spin_thread, (sim,))
